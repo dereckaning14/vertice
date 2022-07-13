77ae14a4cd768cf6a8365f8dc4825ed3860c963f
dereckaning14 <109220680+dereckaning14@users.noreply.github.com>
2022-07-13 13:17:17 +0100

    Add files via upload

---
+++ learn-terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d75513e7706cf2d9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+resource "local_file" hello-world {
+  filename = "${path.module}/home/hello_world.txt"
+  content = "foo"
+  }
